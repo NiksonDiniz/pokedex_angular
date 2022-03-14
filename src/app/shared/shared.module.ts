a52830a3aa224f9db4458d89cf6b73fcaf55a84c
Nikson Diniz <nikson.diniz@gmail.com>
2022-03-14 11:18:10 -0300

    configura a pesquisa html/scss

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 //Components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 @NgModule({
-  declarations: [PokeHeaderComponent, PokeSearchComponent],
-  exports: [PokeHeaderComponent],
+  declarations: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
+  exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
