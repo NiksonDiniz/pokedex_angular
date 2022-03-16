71eace197e200169426bc01a99a7d15743b87e72
Nikson Diniz <nikson.diniz@gmail.com>
2022-03-16 10:07:35 -0300

    configura o search de pokemons

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
 
   ngOnInit(): void {
+    this.pokemon;
   }
 
+  get pokemon() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(
+      `${this.urlPokemon}/${id}`
+    );
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+    return console.log(id);
+  }
 }
